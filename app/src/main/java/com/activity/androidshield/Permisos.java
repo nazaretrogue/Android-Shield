c0a883775d324d01ad4c1a4791e8cd0fa84c2e27
Nazaret <nazaret.ro.gue@gmail.com>
2021-04-16 13:12:16 +0200

    Modificada ruta archivo de datos

--- app/src/main/java/com/activity/androidshield/Permisos.java
+++ app/src/main/java/com/activity/androidshield/Permisos.java
@@ -42,36 +42,36 @@ public class Permisos
 
     private static void ListaPermisosPeligrosos()
     {
-        permisos_peligrosos.add("android.permission.READ_CALENDAR");
-        permisos_peligrosos.add("android.permission.WRITE_CALENDAR");
-        permisos_peligrosos.add("android.permission.CAMERA");
-        permisos_peligrosos.add("android.permission.READ_CONTACTS");
-        permisos_peligrosos.add("android.permission.WRITE_CONTACTS");
-        permisos_peligrosos.add("android.permission.GET_ACCOUNTS");
-        permisos_peligrosos.add("android.permission.ACCESS_FINE_LOCATION");
-        permisos_peligrosos.add("android.permission.ACCESS_COARSE_LOCATION");
-        permisos_peligrosos.add("android.permission.RECORD_AUDIO");
-        permisos_peligrosos.add("android.permission.READ_PHONE_STATE");
-        permisos_peligrosos.add("android.permission.READ_PHONE_NUMBERS");
-        permisos_peligrosos.add("android.permission.CALL_PHONE");
-        permisos_peligrosos.add("android.permission.ANSWER_PHONE_CALLS");
-        permisos_peligrosos.add("android.permission.READ_CALL_LOG");
-        permisos_peligrosos.add("android.permission.WRITE_CALL_LOG");
-        permisos_peligrosos.add("android.permission.ADD_VOICEMAIL");
-        permisos_peligrosos.add("android.permission.USE_SIP");
-        permisos_peligrosos.add("android.permission.PROCESS_OUTGOING_CALLS");
-        permisos_peligrosos.add("android.permission.BODY_SENSORS");
-        permisos_peligrosos.add("android.permission.SEND_SMS");
-        permisos_peligrosos.add("android.permission.RECEIVE_SMS");
-        permisos_peligrosos.add("android.permission.READ_SMS");
-        permisos_peligrosos.add("android.permission.RECEIVE_WAP_PUSH");
-        permisos_peligrosos.add("android.permission.RECEIVE_MMS");
-        permisos_peligrosos.add("android.permission.READ_EXTERNAL_STORAGE");
-        permisos_peligrosos.add("android.permission.WRITE_EXTERNAL_STORAGE");
-        permisos_peligrosos.add("android.permission.ACCESS_MEDIA_LOCATION");
         permisos_peligrosos.add("android.permission.ACCEPT_HANDOVER");
         permisos_peligrosos.add("android.permission.ACCESS_BACKGROUND_LOCATION");
+        permisos_peligrosos.add("android.permission.ACCESS_COARSE_LOCATION");
+        permisos_peligrosos.add("android.permission.ACCESS_FINE_LOCATION");
+        permisos_peligrosos.add("android.permission.ACCESS_MEDIA_LOCATION");
         permisos_peligrosos.add("android.permission.ACTIVITY_RECOGNITION");
+        permisos_peligrosos.add("android.permission.ADD_VOICEMAIL");
+        permisos_peligrosos.add("android.permission.ANSWER_PHONE_CALLS");
+        permisos_peligrosos.add("android.permission.BODY_SENSORS");
+        permisos_peligrosos.add("android.permission.CALL_PHONE");
+        permisos_peligrosos.add("android.permission.CAMERA");
+        permisos_peligrosos.add("android.permission.GET_ACCOUNTS");
+        permisos_peligrosos.add("android.permission.PROCESS_OUTGOING_CALLS");
+        permisos_peligrosos.add("android.permission.READ_CALENDAR");
+        permisos_peligrosos.add("android.permission.READ_CALL_LOG");
+        permisos_peligrosos.add("android.permission.READ_CONTACTS");
+        permisos_peligrosos.add("android.permission.READ_EXTERNAL_STORAGE");
+        permisos_peligrosos.add("android.permission.READ_PHONE_NUMBERS");
+        permisos_peligrosos.add("android.permission.READ_PHONE_STATE");
+        permisos_peligrosos.add("android.permission.READ_SMS");
+        permisos_peligrosos.add("android.permission.RECEIVE_MMS");
+        permisos_peligrosos.add("android.permission.RECEIVE_SMS");
+        permisos_peligrosos.add("android.permission.RECEIVE_WAP_PUSH");
+        permisos_peligrosos.add("android.permission.RECORD_AUDIO");
+        permisos_peligrosos.add("android.permission.SEND_SMS");
+        permisos_peligrosos.add("android.permission.USE_SIP");
+        permisos_peligrosos.add("android.permission.WRITE_CALENDAR");
+        permisos_peligrosos.add("android.permission.WRITE_CALL_LOG");
+        permisos_peligrosos.add("android.permission.WRITE_CONTACTS");
+        permisos_peligrosos.add("android.permission.WRITE_EXTERNAL_STORAGE");
     }
 
 
